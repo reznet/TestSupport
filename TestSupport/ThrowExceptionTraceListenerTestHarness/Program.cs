010f8ef8f9c3c2fca280121e7513bdc1b811ecbf
Jeff Evans <reznet@gmail.com>
2013-06-30 23:01:54 -0700

    add test harness console app for ThrowExceptionTraceListener

---
+++ TestSupport/ThrowExceptionTraceListenerTestHarness/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestSupport;
+
+namespace ThrowExceptionTraceListenerTestHarness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Trace.Fail("FAIL");
+            }
+            catch (AssertFailureException afe)
+            {
+                if (!"FAIL".Equals(afe.Message))
+                {
+                    Console.Error.WriteLine("Wrong message in caught AssertFailureException.  Expected 'FAIL' but got '{0}", afe.Message);
+                    Environment.Exit(1);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("Trace.Fail() did not throw expected exception type.  Expected AssertFailureException, but caught '{0}'", e.GetType());
+                Environment.Exit(2);
+            }
+        }
+    }
+}
